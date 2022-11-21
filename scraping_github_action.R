0e9912f5c8c58eca314fd51ff098568ad5aa1f7f
prdm0 <pedro.rafael.marinho@gmail.com>
2022-11-21 15:30:24 -0300

    Atualizando arquivo com data.

--- scraping_github_action.R
+++ scraping_github_action.R
@@ -2,4 +2,4 @@ source("api_with_proxies.R")
 
 valentina <- scraping(bairro = "valentina-de-figueiredo", tab = FALSE)
 
-save(valentina, file = "valentina_github_action.RData")
+save(valentina, file = glue::glue("valentina_github_action_{Sys.time()}.RData"))
