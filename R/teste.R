c4f23c551e64601a208128829eed65334fa7bef0
prdm0 <pedro.rafael.marinho@gmail.com>
2022-11-23 20:19:57 -0300

    Acho que o erro foi encontrado. Apenas faltava o carregamento da biblioteca pbmcapply.

--- R/teste.R
+++ R/teste.R
@@ -1,5 +1,5 @@
 source("R/scraping/web_scraping_code.R")
 
-cat(try_scraping(bairro = "bessa", tab = FALSE))
+cat(scraping(bairro = "bessa", tab = FALSE))
 
 cat("--> ", parallel::detectCores(), "\n")
