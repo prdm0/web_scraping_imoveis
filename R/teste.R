2c514e8a716c2cd92270e314a15c519d000466db
prdm0 <pedro.rafael.marinho@gmail.com>
2022-11-23 19:55:14 -0300

    Atualizando dados.

--- R/teste.R
+++ R/teste.R
@@ -1,7 +1,5 @@
 source("R/scraping/web_scraping_code.R")
 
-dados <- scraping(bairro = "bessa", tab = FALSE)
+scraping(bairro = "bessa", tab = FALSE)
 
 cat("--> ", parallel::detectCores(), "\n")
-
-print(dados)
